49977a2efa7745b18b18f88cdf0fc559ac8f17e0
Hector Pinto <hectorulises@gmail.com>
2022-12-14 15:46:11 -0300

    Full commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit{
   ngOnInit(): void { this.initialLoad }
 
   initialLoad(){
-    console.log("Initial console.log"); 
+  //  console.log("Initial console.log"); 
   }
 
   constructor(private modalService: NgbModal) {
